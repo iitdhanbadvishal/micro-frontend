9b068013cef7640b7d709e73f88a6c13e68ae30f
Vishal Anand <vishal.anand@agnity.com>
2023-07-09 02:40:08 +0530

    integration of auth with container

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom";
-import { createMemoryHistory, createBrowserHistory } from 'history'
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from "./App"
 
 // Mount function to start of the app
@@ -8,7 +8,7 @@ import App from "./App"
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [initialPath]
-  });;
+  });
 
   if (onNavigate) {
     history.listen(onNavigate)
@@ -32,7 +32,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector('#_marketing-dev-root')
+  const devRoot = document.querySelector('#_auth-dev-root')
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory })
   }
